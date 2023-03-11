22fd3d106f0302fd9361f7ae439f2433c76df097
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-03-11 19:15:00 +0530

    Create Minimize the Heights II.cpp

---
+++ Minimize the Heights II.cpp
@@ -0,0 +1,95 @@
+/*
+Example 1:
+
+Input:
+K = 2, N = 4
+Arr[] = {1, 5, 8, 10}
+Output:
+5
+Explanation:
+The array can be modified as 
+{3, 3, 6, 8}. The difference between 
+the largest and the smallest is 8-3 = 5.
+Example 2:
+
+Input:
+K = 3, N = 5
+Arr[] = {3, 9, 12, 16, 20}
+Output:
+11
+Explanation:
+The array can be modified as
+{6, 12, 9, 13, 17}. The difference between 
+the largest and the smallest is 17-6 = 11. 
+*/
+**********************************************************************************************************************
+
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int getMinDiff(int arr[], int n, int k) {
+        // code here
+        int max1;
+
+        int min1;
+
+        if(n==1){
+
+            return 0;
+
+        }
+
+        sort(arr,arr+n);
+
+        int diff=arr[n-1]-arr[0];
+
+        for(int i=1;i<n;i++){
+
+            if(arr[i]-k<0){
+
+                continue;
+
+            }else{
+
+                max1=max(arr[i-1]+k,arr[n-1]-k);
+
+                min1=min(arr[0]+k,arr[i]-k);
+
+                diff=min(diff,max1-min1);
+
+            }
+
+        }
+
+        return diff;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> k;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getMinDiff(arr, n, k);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
