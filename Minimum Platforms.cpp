98e8577086fd74909ab9f2e6a283c5bed7dc1116
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-03-13 13:18:00 +0530

    Create Minimum Platforms.cpp

---
+++ Minimum Platforms.cpp
@@ -0,0 +1,98 @@
+/*
+Example 1:
+
+Input: n = 6 
+arr[] = {0900, 0940, 0950, 1100, 1500, 1800}
+dep[] = {0910, 1200, 1120, 1130, 1900, 2000}
+Output: 3
+Explanation: 
+Minimum 3 platforms are required to 
+safely arrive and depart all trains.
+
+
+Example 2:
+
+Input: n = 3
+arr[] = {0900, 1100, 1235}
+dep[] = {1000, 1200, 1240}
+Output: 1
+Explanation: Only 1 platform is required to 
+safely manage the arrival and departure 
+of all trains. 
+
+*/
+************************************************************************************************************************************
+
+
+//{ Driver Code Starts
+// Program to find minimum number of platforms
+// required on a railway station
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    int findPlatform(int arr[], int dep[], int n)
+    {
+    	// Your code here
+    	   sort(arr,arr+n);
+
+     sort(dep,dep+n);
+
+     int i=1;
+
+     int j=0;
+
+     int count=1;
+
+     while(i<n && j<n){
+
+         if(arr[i]<=dep[j]){
+
+             count++;
+
+         }
+
+         else{
+
+             j++;
+
+         }
+
+         i++;
+
+     }
+
+     return count;
+
+    }
+};
+
+
+//{ Driver Code Starts.
+// Driver code
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        int dep[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        for(int j=0;j<n;j++){
+            cin>>dep[j];
+        }
+        Solution ob;
+        cout <<ob.findPlatform(arr, dep, n)<<endl;
+    } 
+   return 0;
+}
+// } Driver Code Ends
