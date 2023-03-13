1fecdd52bf746e15dda0c9839a76ad50648c2527
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-03-13 13:20:36 +0530

    Create Peak element.cpp

---
+++ Peak element.cpp
@@ -0,0 +1,102 @@
+/*
+Example 1:
+
+Input: 
+N = 3
+arr[] = {1,2,3}
+Possible Answer: 2
+Generated Output: 1
+Explanation: index 2 is 3.
+It is the peak element as it is 
+greater than its neighbour 2.
+If 2 is returned then the generated output will be 1 else 0.
+
+
+Example 2:
+
+Input:
+N = 2
+arr[] = {3,4}
+Possible Answer: 1
+Output: 1
+Explanation: 4 (at index 1) is the 
+peak element as it is greater than 
+its only neighbour element 3.
+If 1 is returned then the generated output will be 1 else 0.
+*/
+
+*****************************************************************************************************************
+  //{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/* The function should return the index of any
+   peak element present in the array */
+
+// arr: input array
+// n: size of array
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n)
+    {
+       // Your code here
+        int maxi = *max_element(arr, arr+n);
+
+       for(int i = 0; i<n; i++){
+
+           if(arr[i] == maxi){
+
+               return i;
+
+           }
+
+       }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n], tmp[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+			tmp[i] = a[i];
+		}
+		bool f=0;
+		Solution ob;
+		
+		int A = ob. peakElement(tmp,n);
+		
+		if(A<0 and A>=n)
+		    cout<<0<<endl;
+		else
+		{
+    		if(n==1 and A==0)
+    		    f=1;
+    		else if(A==0 and a[0]>=a[1])
+    		    f=1;
+    		else if(A==n-1 and a[n-1]>=a[n-2])
+    		    f=1;
+    		else if(a[A]>=a[A+1] and a[A]>= a[A-1])
+    		    f=1;
+    		else
+    		    f=0;
+    		cout<<f<<endl;
+		}
+		
+	}
+
+	return 0;
+}
+// } Driver Code Ends
+  
