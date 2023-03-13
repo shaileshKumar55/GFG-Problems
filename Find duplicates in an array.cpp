2416b0801324fe3f0e811a800dd86b284ac1d691
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-03-13 13:09:04 +0530

    Create Find duplicates in an array.cpp

---
+++ Find duplicates in an array.cpp
@@ -0,0 +1,85 @@
+/*
+Example 1:
+
+Input:
+N = 4
+a[] = {0,3,1,2}
+Output: -1
+Explanation: N=4 and all elements from 0
+to (N-1 = 3) are present in the given
+array. Therefore output is -1.
+Example 2:
+
+Input:
+N = 5
+a[] = {2,3,1,2,3}
+Output: 2 3 
+Explanation: 2 and 3 occur more than once
+in the given array.
+*/
+****************************************************************************************************************************
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    vector<int> duplicates(int arr[], int n) {
+        // code here
+        vector<int> ans;
+
+    
+
+    for(int i=0;i<n;i++){
+
+        
+
+        int index=arr[i]%n;
+
+        arr[index]=arr[index] + n;//ye duplicate ka btayega baad me
+
+        
+
+    }
+
+    
+
+    for(int i=0;i<n;i++){
+
+        
+
+        if((arr[i]/n) >= 2){
+
+        ans.push_back(i);    
+
+        }
+
+    }
+
+    if(ans.size()==0)
+        return {-1};
+    return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution obj;
+        vector<int> ans = obj.duplicates(a, n);
+        for (int i : ans) cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
