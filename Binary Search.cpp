037c9b9b49c2247afe0ce409bedae4ba47c4904f
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-03-13 13:15:44 +0530

    Create Binary Search.cpp

---
+++ Binary Search.cpp
@@ -0,0 +1,72 @@
+/*
+Example 1:
+
+Input:
+N = 5
+arr[] = {1 2 3 4 5} 
+K = 4
+Output: 3
+Explanation: 4 appears at index 3.
+
+Example 2:
+
+Input:
+N = 5
+arr[] = {11 22 33 44 55} 
+K = 445
+Output: -1
+Explanation: 445 is not present.
+*/ 
+************************************************************************************************************************
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+         int startIndex = 0;
+        int endIndex = n-1;
+        while(startIndex<=endIndex){
+            int mid = (startIndex+endIndex)/2;
+            if(arr[mid]<k){
+                startIndex = mid+1;
+            }
+            else if(arr[mid]>k){
+                endIndex = mid-1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++) cin >> arr[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        int found = ob.binarysearch(arr, N, key);
+        cout << found << endl;
+    }
+}
+
+// } Driver Code Ends
