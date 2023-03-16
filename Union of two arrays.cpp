91604dac2a6ad0379b218ba32c2a62a0705deb7d
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-03-16 16:01:59 +0530

    Create Union of two arrays.cpp

---
+++ Union of two arrays.cpp
@@ -0,0 +1,104 @@
+/*
+Example 1:
+
+Input:
+5 3
+1 2 3 4 5
+1 2 3
+Output: 
+5
+Explanation: 
+1, 2, 3, 4 and 5 are the
+elements which comes in the union set
+of both arrays. So count is 5.
+
+
+
+Example 2:
+
+Input:
+6 2 
+85 25 1 32 54 6
+85 2 
+Output: 
+7
+Explanation: 
+85, 25, 1, 32, 54, 6, and
+2 are the elements which comes in the
+union set of both arrays. So count is 7.
+*/
+
+**********************************************************************************************
+
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template in C++
+
+class Solution{
+    public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int a[], int n, int b[], int m)  {
+        //code here
+           int arr[n+m];
+
+        for(int i=0;i<n;i++){
+
+            arr[i]=a[i];
+
+        }
+
+        for(int i=0;i<m;i++){
+
+            arr[i+n]=b[i];
+
+        }
+
+        sort(arr,arr+(n+m));
+
+        int count =0;
+
+        for(int i=0;i<(n+m-1);i++){
+
+            if(arr[i]==arr[i+1]){
+
+                count++;
+
+            }
+
+        }
+
+        return n+m-count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, m;
+	    cin >> n >> m;
+	    int a[n], b[m];
+	   
+	    for(int i = 0;i<n;i++)
+	       cin >> a[i];
+	       
+	    for(int i = 0;i<m;i++)
+	       cin >> b[i];
+	    Solution ob;
+	    cout << ob.doUnion(a, n, b, m) << endl;
+	    
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
