3dfb1214af72917b05477ec113bff3d42ebba725
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-03-19 17:01:37 +0530

    Create Anagram.cpp

---
+++ Anagram.cpp
@@ -0,0 +1,65 @@
+/*
+Example 1:
+
+Input:a = geeksforgeeks, b = forgeeksgeeks
+Output: YES
+Explanation: Both the string have same characters with
+        same frequency. So, both are anagrams.
+        
+        
+Example 2:
+
+Input:a = allergy, b = allergic
+Output: NO
+Explanation: Characters in both the strings are 
+        not same, so they are not anagrams.
+*/
+
+*******************************************************************************************************************************************************
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function is to check whether two strings are anagram of each other or not.
+    bool isAnagram(string a, string b){
+        
+        // Your code here
+        sort(a.begin(),a.end());
+
+        sort(b.begin(),b.end());
+
+        
+
+        return a==b;
+       
+        
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    
+    int t;
+
+    cin >> t;
+
+    while(t--){
+        string c, d;
+
+        cin >> c >> d;
+        Solution obj;
+        if(obj.isAnagram(c, d)) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+
+}
+
+// } Driver Code Ends
