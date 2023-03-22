d0d56c77dfadfe3d7fe8f9504f6f7cde257cf579
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-03-22 11:09:14 +0530

    Create Rotate Array.cpp

---
+++ Rotate Array.cpp
@@ -0,0 +1,115 @@
+/*
+Given an unsorted array arr[] of size N. Rotate the array to the left (counter-clockwise direction) by D steps, where D is a positive integer. 
+
+ 
+
+Example 1:
+
+Input:
+N = 5, D = 2
+arr[] = {1,2,3,4,5}
+Output: 3 4 5 1 2
+Explanation: 1 2 3 4 5  when rotated
+by 2 elements, it becomes 3 4 5 1 2.
+
+
+Example 2:
+
+Input:
+N = 10, D = 3
+arr[] = {2,4,6,8,10,12,14,16,18,20}
+Output: 8 10 12 14 16 18 20 2 4 6
+Explanation: 2 4 6 8 10 12 14 16 18 20 
+when rotated by 3 elements, it becomes 
+8 10 12 14 16 18 20 2 4 6.
+ 
+*/
+
+
+***************************************************************************************************************************************
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    void rotateArr(int arr[], int d, int n){
+        // code here
+          if(d>n)
+
+        {
+
+          while(d>n)
+
+          {
+
+              d=d-n;
+
+          }
+
+        }
+         vector<int>temp;
+
+        for(int i=d;i<n;i++)
+
+        {
+
+            temp.push_back(arr[i]);
+
+        }
+
+        for(int i=0;i<d;i++)
+
+        {
+
+            temp.push_back(arr[i]);
+
+        }
+
+        for(int i=0;i<n;i++)
+
+        {
+
+            arr[i]=temp[i];
+
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	//taking testcases
+	cin >> t;
+	
+	while(t--){
+	    int n, d;
+	    
+	    //input n and d
+	    cin >> n >> d;
+	    
+	    int arr[n];
+	    
+	    //inserting elements in the array
+	    for(int i = 0; i < n; i++){
+	        cin >> arr[i];
+	    }
+	    Solution ob;
+	    //calling rotateArr() function
+	    ob.rotateArr(arr, d,n);
+	    
+	    //printing the elements of the array
+	    for(int i =0;i<n;i++){
+	        cout << arr[i] << " ";
+	    }
+	    cout << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
