c72c750647a1158ec831b685a471e3214a8140b6
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-03-24 15:48:19 +0530

    Create Display longest name.cpp

---
+++ Display longest name.cpp
@@ -0,0 +1,68 @@
+/*
+Given a list of names, display the longest name.
+
+
+Example:
+
+Input:
+N = 5
+names[] = { "Geek", "Geeks", "Geeksfor",
+  "GeeksforGeek", "GeeksforGeeks" }
+
+Output:
+GeeksforGeeks
+ 
+*/
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    string longest(string names[], int n)
+    {
+            string max=names[0];
+
+        
+
+        for(int i=0;i<n;i++)
+
+        {
+
+            if(names[i].length()>max.length())
+
+            max=names[i];
+
+        }
+
+        return max;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		string names[n];
+		
+		for(int i=0;i<n;i++)
+			cin>>names[i];
+		Solution ob;
+		cout<< ob.longest(names, n) << endl;
+	}
+}
+
+// } Driver Code Ends
