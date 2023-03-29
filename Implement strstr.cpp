28e3e780a3b4d9b10386affa5a3101806d8831ca
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-03-29 16:45:00 +0530

    Create Implement strstr.cpp

---
+++ Implement strstr.cpp
@@ -0,0 +1,60 @@
+/*
+Example 1:
+
+Input:
+s = GeeksForGeeks, x = Fr
+Output: -1
+Explanation: Fr is not present in the
+string GeeksForGeeks as substring.
+ 
+
+Example 2:
+
+Input:
+s = GeeksForGeeks, x = For
+Output: 5
+Explanation: For is present as substring
+in GeeksForGeeks from index 5 (0 based
+indexing).
+*/
+
+
+************************************************************************************************************************
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int strstr(string ,string);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a;
+        string b;
+        
+        cin>>a;
+        cin>>b;
+        
+        cout<<strstr(a,b)<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+//Function to locate the occurrence of the string x in the string s.
+int strstr(string s, string x)
+{
+     //Your code here
+     if(s.find(x)!=-1){
+
+        return s.find(x);
+
+}
+
+     return -1;
+}
