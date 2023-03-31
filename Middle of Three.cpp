56d799eba8f36097a51b7486e69337cbb3f41f6e
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-03-31 16:37:38 +0530

    Create Middle of Three.cpp

---
+++ Middle of Three.cpp
@@ -0,0 +1,63 @@
+/*
+Given three distinct numbers A, B and C. Find the number with value in middle (Try to do it with minimum comparisons).
+
+
+Example 1:
+
+Input:
+A = 978, B = 518, C = 300
+Output:
+518
+Explanation:
+Since 518>300 and 518<978, so 
+518 is the middle element.
+
+Example 2:
+
+Input:
+A = 162, B = 934, C = 200
+Output:
+200
+Exaplanation:
+Since 200>162 && 200<934,
+So, 200 is the middle element.
+*/
+
+_____________________________________________________________________________________________________________________________________
+  
+  
+  //{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int middle(int A, int B, int C){
+        //code here//Position this line where user code will be pasted.
+        int maxx = max(A, max(B, C));
+        int minn = min(A, min(B, C));
+        
+        return (A + B + C) - (maxx + minn);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A,B,C;
+        cin>>A>>B>>C;
+        Solution ob;
+        cout<<ob.middle(A,B,C) <<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
