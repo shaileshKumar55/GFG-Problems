9863f01070fe6949648c5041ec8804e27af9bc32
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-04-09 22:27:18 +0530

    Create Nth Natural Number.cpp

---
+++ Nth Natural Number.cpp
@@ -0,0 +1,70 @@
+/*
+Given a positive integer N. You have to find Nth natural number after removing all the numbers containing digit 9.
+
+
+Example 1:
+
+Input:
+N = 8
+Output:
+8
+Explanation:
+After removing natural numbers which contains
+digit 9, first 8 numbers are 1,2,3,4,5,6,7,8
+and 8th number is 8.
+
+
+Example 2:
+
+Input:
+N = 9
+Output:
+10
+Explanation:
+After removing natural numbers which contains
+digit 9, first 9 numbers are 1,2,3,4,5,6,7,8,10
+and 9th number is 10.
+*/
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+class Solution{
+	public:
+    	long long findNth(long long N)
+    {
+        // code here.
+         long long ans=0,x=1;
+          while(N){
+            ans += (N%9)*x;
+            N/=9;
+            x*=10;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long n , ans;
+		cin>>n;
+		Solution obj;
+		ans = obj.findNth(n);
+		cout<<ans<<endl;
+	}
+}
+
+
+// } Driver Code Ends
