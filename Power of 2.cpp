0db4750062a6bd5879560730c7d3dd3f1dd0286d
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-04-11 21:31:57 +0530

    Create Power of 2.cpp

---
+++ Power of 2.cpp
@@ -0,0 +1,71 @@
+/*
+Given a non-negative integer N. The task is to check if N is a power of 2. More formally, check if N can be expressed as 2x for some x.
+
+Example 1:
+
+Input: N = 1
+Output: YES
+Explanation:1 is equal to 2 
+raised to 0 (20 = 1).
+
+Example 2:
+
+Input: N = 98
+Output: NO
+Explanation: 98 cannot be obtained
+by any power of 2.
+*/
+_______________________________________________________________________________________________________________________________________________
+  
+  //{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        // Your code here    
+            if(n<=0) 
+        return false;
+        else if ( (n&(n-1))==0)
+        return true;
+        else 
+        return false;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+
+    int t;
+    cin>>t;//testcases
+
+    for(int i=0;i<t;i++)
+    {
+        long long n; //input a number n
+        cin>>n;
+
+        Solution ob;
+         if(ob.isPowerofTwo(n))//Now, if log2 produces an integer not decimal then we are sure raising 2 to this value
+             cout<<"YES"<<endl;
+         else
+            cout<<"NO"<<endl;
+
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
