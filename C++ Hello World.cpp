7998b9789a0824731f03ac76f265f49e92b34974
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-04-15 16:36:54 +0530

    Create C++ Hello World.cpp

---
+++ C++ Hello World.cpp
@@ -0,0 +1,40 @@
+/*
+Let's begin your journey towards coding with the very first question of coding world. Your task is to write code which prints Hello World.
+
+Example 1:
+
+Input:
+No Input
+Output:
+Hello World
+
+*/
+
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    void helloWorld()
+    {
+    
+        //Type below cout<<"Hello World"; 
+        //cout will output to console
+        cout<<"Hello World"; 
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    Solution ob;
+    //function call
+    ob.helloWorld();
+    cout<<endl;
+    return 0;
+}
+// } Driver Code Ends
