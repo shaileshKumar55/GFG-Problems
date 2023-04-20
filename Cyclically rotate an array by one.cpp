c9087112f8b4127493f47e827f3e3ee3d501f54e
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-04-20 19:32:33 +0530

    Create Cyclically rotate an array by one.cpp

---
+++ Cyclically rotate an array by one.cpp
@@ -0,0 +1,73 @@
+/*
+Given an array, rotate the array by one position in clock-wise direction.
+ 
+
+Example 1:
+
+Input:
+N = 5
+A[] = {1, 2, 3, 4, 5}
+Output:
+5 1 2 3 4
+ 
+
+Example 2:
+
+Input:
+N = 8
+A[] = {9, 8, 7, 6, 4, 2, 1, 3}
+Output:
+3 9 8 7 6 4 2 1
+*/
+
+
+*******************************************************************************************************************************************************************
+
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+void rotate(int arr[], int n);
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        int a[n] , i;
+        for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        rotate(a, n);
+        for (i = 0; i < n; i++)
+            printf("%d ", a[i]);
+        printf("\n");
+    }
+        return 0;
+}
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+void rotate(int arr[], int n)
+{
+    int i, temp;
+
+    for(i=1;i<n;i++)
+
+    {
+
+        temp = arr[i];
+
+        arr[i] = arr[0];
+
+        arr[0] = temp;
+
+    }
+}
