961e642f38516e0c8c1c95f0fc9c0f25247361b4
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-04-25 19:35:02 +0530

    Create Max min Height.cpp

---
+++ Max min Height.cpp
@@ -0,0 +1,114 @@
+/*
+You have a garden with n flowers lined up in a row. The height of ith flower is ai units. You will water them for k days. In one day you can water w continuous flowers (you can do this only once in a single day). Whenever you water a flower its height increases by 1 unit. You need to maximize the height of the smallest flower all the time.
+
+Example 1:
+
+Input:
+n=6
+k=2
+w=3
+a[]={2,2,2,2,1,1}
+Output:
+2
+Explanation:
+Water last three flowers for first day & 
+first three flowers for second day.The 
+new heights will be {3,3,3,3,2,2}
+
+
+Example 2:
+
+Input:
+n=2
+k=5
+w=1
+a[]={5,8}
+Output:
+9
+Explanation:
+For the first four days water the first flower then
+water the last flower once.
+*/
+
+****************************************************************************************************************************
+
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+    public:
+        bool isCheck(vector<int> &arr, int days, int cs, long long int h){
+            
+            int n=arr.size();
+            vector<long long> waterSupply(n,0);
+            if(arr[0]<h){
+                waterSupply[0]=h-arr[0];
+                days-=(h-arr[0]);
+            }
+            if(days<0){
+                return false;
+            }
+            for(int i=1;i<n;i++){
+                waterSupply[i]=waterSupply[i-1];
+                int actualHeight=arr[i];
+                if(i>=cs){
+                    actualHeight+=(waterSupply[i]-waterSupply[i-cs]);
+                }
+                else{
+                    actualHeight+=waterSupply[i];
+                }
+                if(actualHeight<h){
+                    waterSupply[i]+=(h-actualHeight);
+                    days-=(h-actualHeight);
+                }
+                if(days<0){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        long long int maximizeMinHeight(int n,int k,int w,vector <int> &a){
+            long long int ans=-1;
+            long long int mini=*min_element(a.begin(),a.end());
+            long long int maxi=INT_MAX/2;
+            while(mini<=maxi){
+                int guessHeight=(maxi+mini)/2;
+                if(isCheck(a,k,w,guessHeight)==true){
+                    ans=guessHeight;
+                    mini=guessHeight+1;
+                }
+                else{
+                    maxi=guessHeight-1;
+                }
+            }
+            return ans;  
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k,w;
+        cin>>n>>k>>w;
+        vector <int> a(n);
+        for(auto &j:a)
+            cin>>j;
+        Solution s;
+        cout<<s.maximizeMinHeight(n,k,w,a)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
