6bf6ff858b6dcfc62c7dead5e0b64bbb821a9507
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-04-28 22:36:12 +0530

    Create Find the element that appears once in sorted array.cpp

---
+++ Find the element that appears once in sorted array.cpp
@@ -0,0 +1,67 @@
+/*
+Given a sorted array arr[] of size N. Find the element that appears only once in the array. All other elements appear exactly twice. 
+
+Example 1:
+
+Input:
+N = 11
+arr[] = {1, 1, 2, 2, 3, 3, 4, 50, 50, 65, 65}
+Output: 4
+Explanation: 4 is the only element that 
+appears exactly once.
+*/
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+  //{ Driver Code Starts
+// Driver code
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int findOnce(int arr[], int n)
+    {
+        //code here.
+          for(int i=0;i<n;i=i+2){
+
+            if(arr[i]!=arr[i+1]){
+
+                return arr[i];
+
+            }
+
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int A[n];
+        for(int i = 0;i < n;i++)
+        {
+            cin>>A[i];
+        }
+        
+        Solution ob;
+        
+        int res = ob.findOnce(A,n);
+        cout << res << endl;
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
