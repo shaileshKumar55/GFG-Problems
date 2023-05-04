ffa9d45ee3827dcc9b17ef0c16c908683173ced1
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-05-04 20:17:00 +0530

    Create N meetings in one room.cpp

---
+++ N meetings in one room.cpp
@@ -0,0 +1,90 @@
+/*
+There is one meeting room in a firm. There are N meetings in the form of (start[i], end[i]) where start[i] is start time of meeting i and end[i] is finish time of meeting i.
+What is the maximum number of meetings that can be accommodated in the meeting room when only one meeting can be held in the meeting room at a particular time?
+
+Note: Start time of one chosen meeting can't be equal to the end time of the other chosen meeting.
+
+
+Example 1:
+
+Input:
+N = 6
+start[] = {1,3,0,5,8,5}
+end[] =  {2,4,6,7,9,9}
+Output: 
+4
+Explanation:
+Maximum four meetings can be held with
+given start and end timings.
+The meetings are - (1, 2),(3, 4), (5,7) and (8,9)
+
+
+Example 2:
+
+Input:
+N = 3
+start[] = {10, 12, 20}
+end[] = {20, 25, 30}
+Output: 
+1
+Explanation:
+Only one meetings can be held
+with given start and end timings.
+
+*/
+
+*************************************************************************************************************************************************
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    int maxMeetings(int start[], int end[], int n)
+    {
+        // Your code here
+          vector<pair<int,int>> v;
+        for(int i=0;i<n;i++)
+            v.push_back({end[i],start[i]});
+            
+        sort(v.begin(),v.end());
+
+        int last = v[0].first;
+        int task = 1;
+        for(int i=1;i<n;i++)
+        {
+            if(v[i].second > last)
+            {
+                task++;
+                last = v[i].first;
+            }
+        }
+        
+        return task;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++) cin >> start[i];
+
+        for (int i = 0; i < n; i++) cin >> end[i];
+
+        Solution ob;
+        int ans = ob.maxMeetings(start, end, n);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
