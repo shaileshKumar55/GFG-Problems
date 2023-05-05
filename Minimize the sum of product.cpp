e5dfcf01995f0bad6548518e19f3f964fc98fefe
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-05-05 23:25:26 +0530

    Create Minimize the sum of product.cpp

---
+++ Minimize the sum of product.cpp
@@ -0,0 +1,81 @@
+/*
+You are given two arrays, A and B, of equal size N. The task is to find the minimum value of A[0] * B[0] + A[1] * B[1] + .... + A[N-1] * B[N-1], where shuffling of elements of arrays A and B is allowed.
+
+
+Example 1:
+
+Input:
+N = 3 
+A[] = {3, 1, 1}
+B[] = {6, 5, 4}
+Output:
+23 
+Explanation:
+1*6+1*5+3*4 = 6+5+12
+= 23 is the minimum sum
+ 
+
+Example 2:
+
+Input:
+N = 5
+A[] = {6, 1, 9, 5, 4}
+B[] = {3, 4, 8, 2, 4}
+Output:
+80
+Explanation:
+2*9+3*6+4*5+4*4+8*1
+=18+18+20+16+8
+= 80 is the minimum sum
+*/
+
+**************************************************************************************************************************************
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long int minValue(int a[], int b[], int n)
+    {
+        // Your code goes here
+          if (n==0)
+        return 0;
+    sort(a, a+n);
+    sort(b, b+n);
+
+    int i = 0, j = n-1;
+    long long sum = 0;
+    while(i<n){
+        sum+=a[j]*b[i];
+        i++;
+        j--;
+    }
+
+    return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int n, i;
+         cin>>n;
+         int a[n], b[n];
+         for(i=0;i<n;i++)
+         cin>>a[i];
+         for(i=0;i<n;i++)
+         cin>>b[i];
+         Solution ob;
+         cout<< ob.minValue(a, b, n) <<endl;
+     }
+	
+	return 0;
+}
+// } Driver Code Ends
