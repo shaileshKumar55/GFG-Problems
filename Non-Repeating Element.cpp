e164fbc7ddf5c354964c469cc940c0026b09c82b
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-05-07 22:35:04 +0530

    Create Non-Repeating Element.cpp

---
+++ Non-Repeating Element.cpp
@@ -0,0 +1,74 @@
+/*
+Find the first non-repeating element in a given array arr of N integers.
+Note: Array consists of only positive and negative integers and not zero.
+
+Example 1:
+
+Input : arr[] = {-1, 2, -1, 3, 2}
+Output : 3
+Explanation:
+-1 and 2 are repeating whereas 3 is 
+the only number occuring once.
+Hence, the output is 3. 
+ 
+
+Example 2:
+
+Input : arr[] = {1, 1, 1}
+Output : 0
+
+*/
+************************************************************************************************************************************
+
+//{ Driver Code Starts
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int firstNonRepeating(int arr[], int n) 
+    { 
+        // Complete the function
+          unordered_map<int, int> m;
+        for(int i=0;i<n;i++){
+            m[arr[i]]++;
+        }
+        
+        for(int i=0;i<n;i++){
+            if(m[arr[i]] == 1){
+                return arr[i];
+            }
+        }
+        
+        return 0;
+        
+    } 
+  
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for (int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.firstNonRepeating(arr,n)<<endl;
+    }
+}
+
+// } Driver Code Ends
