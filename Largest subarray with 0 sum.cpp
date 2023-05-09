5d5888a5378ae465f68d4314fb4e3fabe1b1daa9
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-05-09 19:56:11 +0530

    Create Largest subarray with 0 sum.cpp

---
+++ Largest subarray with 0 sum.cpp
@@ -0,0 +1,71 @@
+/*
+Given an array having both positive and negative integers. The task is to compute the length of the largest subarray with sum 0.
+
+Example 1:
+
+Input:
+N = 8
+A[] = {15,-2,2,-8,1,7,10,23}
+Output: 5
+Explanation: The largest subarray with
+sum 0 will be -2 2 -8 1 7.
+*/
+
+****************************************************************************************************************************************
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*You are required to complete this function*/
+
+class Solution{
+    public:
+    int maxLen(vector<int>&A, int n)
+    {   
+        // Your code here
+         unordered_map<int,int> map;
+        int ans=0;
+        int sum=0;
+        map[0]=-1;
+        for(int i=0;i<n;i++){
+            sum += A[i];
+            if(map.find(sum) != map.end()){
+                ans = max(ans,i-map[sum]);
+            }
+            else{
+                map[sum]=i;
+            }
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m;
+        cin>>m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i){
+            cin>>array1[i];
+        }
+        Solution ob;
+        cout<<ob.maxLen(array1,m)<<endl;
+    }
+    return 0; 
+}
+
+
+
+// } Driver Code Ends
