3bf0600839ad8e0b4024a295dbe8e57e7dfa49f7
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-05-11 23:03:01 +0530

    Create Equilibrium Point.cpp

---
+++ Equilibrium Point.cpp
@@ -0,0 +1,93 @@
+/*
+Given an array A of n positive numbers. The task is to find the first Equilibrium Point in an array. 
+Equilibrium Point in an array is a position such that the sum of elements before it is equal to the sum of elements after it.
+
+Note: Retun the index of Equilibrium point. (1-based index)
+
+Example 1:
+
+Input: 
+n = 5 
+A[] = {1,3,5,2,2} 
+Output: 3 
+Explanation:  
+equilibrium point is at position 3 
+as elements before it (1+3) = 
+elements after it (2+2). 
+ 
+
+Example 2:
+
+Input:
+n = 1
+A[] = {1}
+Output: 1
+Explanation:
+Since its the only element hence
+its the only equilibrium point.
+*/
+
+**************************************************************************************************************************************
+
+//{ Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to find equilibrium point in the array.
+    // a: input array
+    // n: size of array
+    int equilibriumPoint(long long a[], int n) {
+    
+        // Your code here
+          long sum = 0, rightSum = 0;
+        for(int i=0; i<n; i++)
+        {
+            sum += a[i];
+        }
+        for(int i=0; i<n; i++){
+            sum -= a[i];
+            if(sum == rightSum){
+                return i+1;
+            }
+            rightSum += a[i];
+        }
+        return -1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+
+int main() {
+
+    long long t;
+    
+    //taking testcases
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        
+        //taking input n
+        cin >> n;
+        long long a[n];
+
+        //adding elements to the array
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        
+        Solution ob;
+
+        //calling equilibriumPoint() function
+        cout << ob.equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
