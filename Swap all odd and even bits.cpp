acf889d54603e40b61871304d90fb804bf2eaa70
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-05-14 21:48:24 +0530

    Create Swap all odd and even bits.cpp

---
+++ Swap all odd and even bits.cpp
@@ -0,0 +1,69 @@
+/*
+Given an unsigned integer N. The task is to swap all odd bits with even bits. For example, if the given number is 23 (00010111), it should be converted to 43(00101011). Here, every even position bit is swapped with an adjacent bit on the right side(even position bits are highlighted in the binary representation of 23), and every odd position bit is swapped with an adjacent on the left side.
+
+Example 1:
+
+Input: N = 23
+Output: 43
+Explanation: 
+Binary representation of the given number 
+is 00010111 after swapping 
+00101011 = 43 in decimal.
+Example 2:
+
+Input: N = 2
+Output: 1
+Explanation: 
+Binary representation of the given number 
+is 10 after swapping 01 = 1 in decimal.
+
+*/
+
+****************************************************************************************************************************************
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to swap odd and even bits.
+    unsigned int swapBits(unsigned int n)
+    {
+    	// Your code here
+    	int out=0;
+    	for(int i=0;i<=31;i+=2){
+    	    
+    	   if(n & 1) out+=pow(2,i+1);
+    	   if((n>>1)&1)out+=pow(2,i);
+    	   n=n>>2;
+    	}
+    	return out;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		unsigned int n;
+		cin>>n;//input n
+		
+		Solution ob;
+		//calling swapBits() method
+		cout << ob.swapBits(n) << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
