101dd32ba56005c361a73172d999dab01b0ce40e
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-05-16 21:54:48 +0530

    Create Find first set bit.cpp

---
+++ Find first set bit.cpp
@@ -0,0 +1,67 @@
+/*
+Given an integer N. The task is to return the position of first set bit found from the right side in the binary representation of the number.
+Note: If there is no set bit in the integer N, then return 0 from the function.  
+
+Example 1:
+
+Input: N = 18
+Output: 2
+Explanation: Binary representation of 
+18 is 010010,the first set bit from the 
+right side is at position 2.
+Example 2:
+
+Input: N = 12 
+Output: 3 
+Explanation: Binary representation 
+of  12 is 1100, the first set bit 
+from the right side is at position 3.
+
+*/
+*************************************************************************************************************************************
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find position of first set bit in the given number.
+    unsigned int getFirstSetBit(int n)
+    {
+        // Your code here
+           int b = -n;
+        int c = n&b;
+        if(c==0)
+           return 0;
+        
+        return log2(c)+1;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; // testcases
+    while(t--)
+    {
+        int n;
+        cin>>n; //input n
+        Solution ob;
+        printf("%u\n", ob.getFirstSetBit(n)); // function to get answer
+    }
+	return 0;
+}
+
+// } Driver Code Ends
