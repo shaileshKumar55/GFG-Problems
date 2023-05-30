7f2c80eb5ac1883afb1c8d3ae83b9fc65d0cc766
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-05-30 22:21:00 +0530

    Create Minimum Integer.cpp

---
+++ Minimum Integer.cpp
@@ -0,0 +1,98 @@
+/*
+You are given an array A of size N. Let us denote S as the sum of all integers present in the array. Among all integers present in the array, find the minimum integer X such that S ≤ N*X.
+
+Example 1:
+
+Input:
+N = 3,
+A = { 1, 3, 2}
+Output:
+2
+Explanation:
+Sum of integers in the array is 6.
+since 6 ≤ 3*2, therefore 2 is the answer.
+Example 2:
+
+Input:
+N = 1,
+A = { 3 }
+Output:
+3
+Explanation:
+3 is the only possible answer
+*/
+*********************************************************************************************************************************************
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minimumInteger(int N, vector<int> &A) {
+        // code here
+        int sum=0;
+        vector<long long>v;
+        for(int i=0;i<N;i++){
+            sum=sum+A[i];
+        }
+        for(int i=0;i<N;i++){
+            if(sum<=N*A[i]){
+                v.push_back(A[i]);
+            }
+        }
+        sort(v.begin(),v.end());
+        return v[0];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        Array::input(A,N);
+        
+        Solution obj;
+        int res = obj.minimumInteger(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
