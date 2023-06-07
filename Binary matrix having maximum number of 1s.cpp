3e44a8848d88e68be4f55af972ce1b0100d9dc89
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-06-07 22:19:32 +0530

    Binary matrix having maximum number of 1s.cpp

---
+++ Binary matrix having maximum number of 1s.cpp
@@ -0,0 +1,87 @@
+/*
+Given a binary matrix (containing only 0 and 1) of order NxN. All rows are sorted already, We need to find the row number with the maximum number of 1s. Also, find the number of 1s in that row.
+Note:
+
+1. In case of a tie, print the smaller row number.
+2. Row number should start from 0th index.
+
+Example 1
+
+Input:
+N=3
+mat[3][3] = {0, 0, 1, 
+              0, 1, 1, 
+              0, 0, 0}
+Output:
+Row number = 1
+MaxCount = 2
+Explanation:
+Here, max number of 1s is in row number 1
+and its count is 2.
+Example 2
+
+Input:
+N=3
+mat[3][3] = {1, 1, 1, 
+              1, 1, 1, 
+              0, 0, 0}
+Output:
+Row number = 0
+MaxCount = 3
+*/
+
+*******************************************************************************************************************
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    vector<int> findMaxRow(vector<vector<int>> mat, int N) {
+        //code here
+         int maxi=0, i=0, count=0;
+        for(int j=N-1 ; j>=0 ; j--)
+          {
+              if(i<=N-1 && mat[i][j]==1)
+               {
+                   maxi=i; count++;
+               }
+               else if(i<=N-1)
+                {
+                    i++, j++;
+                }
+          }
+          
+          return {maxi, count};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        vector<vector<int>> arr(n, vector<int> (n));
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                cin >> arr[i][j];
+        Solution obj;
+        vector<int> ans = obj.findMaxRow(arr, n);
+        for(int val : ans) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+// } Driver Code Ends
