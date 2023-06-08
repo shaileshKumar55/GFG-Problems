c4aa5bfdd8241dd7dbee098f8b4702c1d82d6e00
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-06-08 21:32:24 +0530

    Create Rotation.cpp

---
+++ Rotation.cpp
@@ -0,0 +1,72 @@
+/*
+Given an ascending sorted rotated array Arr of distinct integers of size N. The array is right rotated K times. Find the value of K.
+
+Example 1:
+
+Input:
+N = 5
+Arr[] = {5, 1, 2, 3, 4}
+Output: 1
+Explanation: The given array is 5 1 2 3 4. 
+The original sorted array is 1 2 3 4 5. 
+We can see that the array was rotated 
+1 times to the right.
+Example 2:
+
+Input:
+N = 5
+Arr[] = {1, 2, 3, 4, 5}
+Output: 0
+Explanation: The given array is not rotated.
+*/
+
+******************************************************************************************************************************
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int findKRotation(int arr[], int n) {
+	    // code here
+	          int maxi=INT_MIN;
+        int index=-1;
+        for(int i=0;i<n;i++){
+            if(arr[i]>maxi){
+                maxi=arr[i];
+                index=i;
+            }
+        }
+        if(index==n-1){
+            return 0;
+        }
+        return index+1;
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findKRotation(a, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
