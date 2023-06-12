576d02c9de0855d0cd3cc7ecc4546a2707eb8405
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-06-12 22:51:04 +0530

    Create Remove duplicate elements from sorted Array.cpp

---
+++ Remove duplicate elements from sorted Array.cpp
@@ -0,0 +1,78 @@
+/*
+Given a sorted array A[] of size N, delete all the duplicated elements from A[]. Modify the array such that if there are X distinct elements in it then the first X positions of the array should be filled with them in increasing order and return the number of distinct elements in the array.
+
+Note:
+1. Don't use set or HashMap to solve the problem.
+2. You must return the number of distinct elements(X) in the array, the generated output will print all the elements of the modified array from index 0 to X-1.
+
+Example 1:
+
+Input:
+N = 5
+Array = {2, 2, 2, 2, 2}
+Output: {2}
+Explanation: After removing all the duplicates 
+only one instance of 2 will remain.
+Example 2:
+
+Input:
+N = 3
+Array = {1, 2, 2}
+Output: {1, 2}
+*/
+
+**********************************************************************************************************************************
+
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    int remove_duplicate(int a[],int n){
+        // code here
+         int b[n];
+        b[0]=a[0];
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(a[i]!=a[i-1]){
+                b[count++]=a[i];
+                
+            }
+        }
+        for(int i=0;i<count;i++){
+            a[i]=b[i];
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        int a[N];
+        for(int i=0;i<N;i++)
+        {
+            cin>>a[i];
+        }
+    Solution ob;
+    int n = ob.remove_duplicate(a,N);
+
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";
+    cout<<endl;
+    }
+}
+// } Driver Code Ends
