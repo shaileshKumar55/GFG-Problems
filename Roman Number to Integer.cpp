154ef6a25454d1225b36166fb80b3ee0b3629618
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-06-15 22:18:47 +0530

    Create Roman Number to Integer.cpp

---
+++ Roman Number to Integer.cpp
@@ -0,0 +1,72 @@
+/*
+Given a string in roman no format (s)  your task is to convert it to an integer . Various symbols and their values are given below.
+I 1
+V 5
+X 10
+L 50
+C 100
+D 500
+M 1000
+
+Example 1:
+
+Input:
+s = V
+Output: 5
+Example 2:
+
+Input:
+s = III 
+Output: 3
+*/
+
+****************************************************************************************************************************************
+
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    int romanToDecimal(string &str) {
+        // code here
+        unordered_map<char,int> mp;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+        int ans=0;
+        for(int i=0; i < str.length();i++){
+            if(mp[str[i]] < mp[str[i+1]]){
+                ans-=mp[str[i]];
+            }
+            else{
+                ans+=mp[str[i]];
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.romanToDecimal(s) << endl;
+    }
+}
+// } Driver Code Ends
