d905972462b77ed67617cdbf107a981e11c0daf7
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-06-19 22:12:54 +0530

    Create Minimize the sum.cpp

---
+++ Minimize the sum.cpp
@@ -0,0 +1,81 @@
+/*
+You are given N elements, you can remove any two elements from the list, note their sum, and add the sum to the list. Repeat these steps while there is more than a single element in the list. The task is to minimize the sum of these chosen sums.
+
+ 
+
+Example 1:
+
+Input:
+N = 4
+arr[] = {1, 4, 7, 10}
+
+Output:
+39
+
+Explanation:
+Choose 1 and 4, Sum = 1 + 4 = 5.
+arr[] = {5, 7, 10} 
+Choose 5 and 7, Sum = 5 + (5 + 7) = 17.
+arr[] = {12, 10} 
+Choose 12 and 10, Sum = 17 + (12 + 10) = 39.
+arr[] = {22}
+ 
+
+Example 2:
+
+Input:
+N = 5
+arr[] = {1, 3, 7, 5, 6}
+
+Output:
+48
+*/
+*********************************************************************************************************************************************************
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    int minimizeSum(int N, vector<int> arr) {
+        // code here
+        Queue<Integer> pq= new PriorityQueue<>();
+        for(int i=0; i<N; ++i){
+            pq.offer(arr[i]);
+        }
+        int sum=0;
+        while(pq.size()>1){
+            int r1=pq.poll();
+            int r2=pq.poll();
+            sum+=r1+r2;
+            pq.offer(r1+r2);
+            
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i = 0; i < n; i++)
+            cin>>arr[i];
+        Solution obj;
+        cout << obj.minimizeSum(n, arr) << "\n";
+    }
+}
+// } Driver Code Ends
