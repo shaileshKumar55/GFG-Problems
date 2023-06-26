f6af78ac3be8f6585ce76e123f3cc3154ccfab44
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-06-26 22:10:41 +0530

    Create Key Pair.cpp

---
+++ Key Pair.cpp
@@ -0,0 +1,69 @@
+/*
+Given an array Arr of N positive integers and another number X. Determine whether or not there exist two elements in Arr whose sum is exactly X.
+
+Example 1:
+
+Input:
+N = 6, X = 16
+Arr[] = {1, 4, 45, 6, 10, 8}
+Output: Yes
+Explanation: Arr[3] + Arr[4] = 6 + 10 = 16
+Example 2:
+
+Input:
+N = 5, X = 10
+Arr[] = {1, 2, 4, 3, 6}
+Output: Yes
+Explanation: Arr[2] + Arr[4] = 4 + 6 = 10
+*/
+
+*************************************************************************************************************************************************************
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    // code here
+	     Arrays.sort(arr);
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>=x) break;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]>=x) break;
+                if(arr[i]+arr[j]==x){
+                    return true;
+                }
+            }
+        }
+        return false;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, n, x);
+        cout << (ans ? "Yes\n" : "No\n");
+    }
+    return 0;
+}
+
+// } Driver Code Ends
