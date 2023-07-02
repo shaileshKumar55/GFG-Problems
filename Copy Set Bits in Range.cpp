79bef9ac44c08d876afa771df48e1d0649f688e0
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-07-02 22:28:52 +0530

    Create Copy Set Bits in Range.cpp

---
+++ Copy Set Bits in Range.cpp
@@ -0,0 +1,55 @@
+/*
+Given two numbers X and Y, and a range [L, R] where 1 <= L <= R <= 31. You have to copy the set bits of 'Y' in the range L to R in 'X'. Return this modified X.
+
+Note: Range count will be from Right to Left & start from 1.
+
+Example 1:
+
+Input: 
+X = 44, Y = 3 
+L = 1,  R = 5
+Output: 
+47
+Explaination: 
+Binary represenation of 44 and 3 is 101100 and 000011. So in the range 1 to 5 there are two set bits of 3 (1st & 2nd position). If those are set in 44 it will become 101111 which is 47.
+Example 2:
+
+Input: 
+X = 16, Y = 2
+L = 1,  R = 3
+Output: 18
+Explaination: Binary represenation of 16 and 2 is 10000 and 10. If the mentioned conditions are applied then 16 will become 10010 which is 18.
+*/
+
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution{
+    public:
+    int setSetBit(int x, int y, int l, int r){
+        // code here
+         for(int i = l-1; i < r; i++) {
+        x = x | (y & (1 << i));
+    }
+    return x;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x, y, l, r;
+        cin>>x>>y>>l>>r;
+        
+        Solution ob;
+        cout<<ob.setSetBit(x, y, l, r)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
