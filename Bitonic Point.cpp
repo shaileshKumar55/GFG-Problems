2f264543d36a5f12d5616a635353d30a9ef52970
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-07-09 23:04:27 +0530

    Create Bitonic Point.cpp

---
+++ Bitonic Point.cpp
@@ -0,0 +1,66 @@
+/*
+Given an array arr of n elements that is first strictly increasing and then maybe strictly decreasing, find the maximum element in the array.
+Note: If the array is increasing then just print the last element will be the maximum value.
+
+Example 1:
+
+Input: 
+n = 9
+arr[] = {1,15,25,45,42,21,17,12,11}
+Output: 45
+Explanation: Maximum element is 45.
+Example 2:
+
+Input: 
+n = 5
+arr[] = {1, 45, 47, 50, 5}
+Output: 50
+Explanation: Maximum element is 50.
+*/
+***********************************************************************************************************************************************
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	int findMaximum(int arr[], int n) {
+	    // code here
+	           int max=arr[0];
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+        return max;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMaximum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
