8c4d2dd673506a55c59a67f403d8eaaf1e7592e0
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-07-13 21:20:35 +0530

    Create Longest Consecutive 1's.cpp

---
+++ Longest Consecutive 1's.cpp
@@ -0,0 +1,75 @@
+/*
+Given a number N. Find the length of the longest consecutive 1s in its binary representation.
+
+Example 1:
+
+Input: N = 14
+Output: 3
+Explanation: 
+Binary representation of 14 is 
+1110, in which 111 is the longest 
+consecutive set bits of length is 3.
+Example 2:
+
+Input: N = 222
+Output: 4
+Explanation: 
+Binary representation of 222 is 
+11011110, in which 1111 is the 
+longest consecutive set bits of length 4. 
+
+*/
+**************************************************************************************************************************************
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*  Function to calculate the longest consecutive ones
+*   N: given input to calculate the longest consecutive ones
+*/
+class Solution
+{
+    public:
+    int maxConsecutiveOnes(int N)
+    {
+        // code here
+         int maxC = 0;
+        int curr = 0;
+        while(N){
+            if(N&1){
+                ++curr;
+                maxC = max(maxC, curr);
+            } else {
+                curr=0;
+            }
+            N>>=1;
+        }
+        return maxC;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main() {
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n;
+		cin>>n;//input n
+		Solution obj;
+		//calling maxConsecutiveOnes() function
+		cout<<obj.maxConsecutiveOnes(n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
