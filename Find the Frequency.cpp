61ebcda4d81e16e16d21bd306186bdd0538b7668
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-07-17 19:57:35 +0530

    Create Find the Frequency.cpp

---
+++ Find the Frequency.cpp
@@ -0,0 +1,68 @@
+/*
+Given a vector of N positive integers and an integer X. The task is to find the frequency of X in vector.
+
+ 
+
+Example 1:
+
+Input:
+N = 5
+vector = {1, 1, 1, 1, 1}
+X = 1
+Output: 
+5
+Explanation: Frequency of 1 is 5.
+*/
+*********************************************************************************************************************************
+
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*Function to find frequency of x
+* x : element whose frequency is to be found
+* v : input vector
+*/
+int findFrequency(vector<int> v, int x){
+    // Your code here
+        map<int,int>m;
+    for(int i=0;i<v.size();i++){
+        m[v[i]]++;
+    }
+    return m[x];
+}
+
+//{ Driver Code Starts.
+int main() {
+	
+	int testcase;
+	
+	cin >> testcase;
+	
+	while(testcase--){
+	    int N;
+	    cin >> N;
+	    
+	    // Declaring vector 
+	    vector<int> v;
+	        
+	    // Taking vector element input
+	    for(int i = 0;i<N;i++){
+	        int k;
+	        cin >> k;
+	        v.push_back(k);
+	    }
+	    
+	    // element whose frequency to be find
+	    int x;
+	    cin >> x;
+	    cout << findFrequency(v, x) << endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
