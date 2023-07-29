8d5194623b5d375422fcb69402b478f56c9fc2d1
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-07-29 21:15:08 +0530

    Create Median of BST.cpp

---
+++ Median of BST.cpp
@@ -0,0 +1,196 @@
+/*
+Given a Binary Search Tree of size N, find the Median of its Node values.
+
+Example 1:
+
+Input:
+       6
+     /   \
+   3      8   
+ /  \    /  \
+1    4  7    9
+Output: 6
+Explanation: Inorder of Given BST will be:
+1, 3, 4, 6, 7, 8, 9. So, here median will 6.
+
+Example 2:
+
+Input:
+       6
+     /   \
+   3      8   
+ /   \    /   
+1    4   7   
+Output: 5
+Explanation:Inorder of Given BST will be:
+1, 3, 4, 6, 7, 8. So, here median will
+(4 + 6)/2 = 10/2 = 5.
+ 
+*/
+************************************************************************************************************************************************
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+
+float findMedian(struct Node* node);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+ 
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+int main() {
+  
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   //cout << t << endl;
+   while(t--)
+   {
+        string s; 
+       getline(cin, s);
+       Node* root = buildTree(s);
+
+      // getline(cin, s);
+       
+        cout << findMedian(root) << endl;
+
+      // cout<<"~"<<endl;
+   }
+   return 0;
+}
+// } Driver Code Ends
+
+
+/*
+Structure of the binary Search Tree is as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+// your task is to complete the Function
+// Function should return median of the BST
+
+int countnode(Node* root){
+    if(!root){
+        return 0;
+    }
+    else{
+        return 1 + countnode(root->left) + countnode(root->right);
+    }
+}
+void findnode(Node* root, int &c, int &val){
+    if(!root){
+        return;
+    }
+    else{
+        findnode(root->left, c, val);
+        c--;
+        if(c == 0){
+            val = root->data;
+        }
+        findnode(root->right, c, val);
+    }
+}
+float findMedian(struct Node *root)
+{
+      //Code here
+      int N = countnode(root);
+      if(N % 2){
+          int val = -1, c = (N + 1) / 2;
+          findnode(root, c, val);
+          return (float)val;
+      }
+      else{
+          int val = -1,val2 = -1,c = N / 2;
+          findnode(root, c, val);
+          c = N / 2 + 1;
+          findnode(root, c, val2);
+          float ans = (float)(val + val2) / (2.0);
+          return ans;
+      }
+}
+
