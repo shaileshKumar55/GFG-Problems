fa6d80632e197bf8792c9cbafe7db5394d8878e8
Shailesh Kumar <107577858+shaileshKumar55@users.noreply.github.com>
2023-08-05 21:32:32 +0530

    Create Smallest Positive missing number.cpp

---
+++ Smallest Positive missing number.cpp
@@ -0,0 +1,85 @@
+/*
+You are given an array arr[] of N integers. The task is to find the smallest positive number missing from the array.
+
+Note: Positive number starts from 1.
+
+Example 1:
+
+Input:
+N = 5
+arr[] = {1,2,3,4,5}
+Output: 6
+Explanation: Smallest positive missing 
+number is 6.
+Example 2:
+
+Input:
+N = 5
+arr[] = {0,-10,1,3,-20}
+Output: 2
+Explanation: Smallest positive missing 
+number is 2.
+*/
+****************************************************************************************************************************************************
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to find the smallest positive number missing from the array.
+    int missingNumber(int arr[], int n) 
+    { 
+        // Your code here
+        unordered_set<int> s;
+        int maxi = arr[0];
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i] >= 0)
+            {
+                s.insert(arr[i]);
+            }
+            maxi = max(maxi,arr[i]);
+        }
+        for(int i=1;i<=(maxi+1);i++)
+        {
+            if(s.find(i) == s.end())
+            {
+                return i;
+            }
+        }
+        return 1;
+    } 
+};
+
+//{ Driver Code Starts.
+
+int missingNumber(int arr[], int n);
+
+int main() { 
+    
+    //taking testcases
+    int t;
+    cin>>t;
+    while(t--){
+        
+        //input number n
+        int n;
+        cin>>n;
+        int arr[n];
+        
+        //adding elements to the array
+        for(int i=0; i<n; i++)cin>>arr[i];
+        
+        Solution ob;
+        //calling missingNumber()
+        cout<<ob.missingNumber(arr, n)<<endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
